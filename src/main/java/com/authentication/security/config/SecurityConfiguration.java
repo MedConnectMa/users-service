cf0d4f68a3452d81b9707ccae315d828eae41d7b
KhOussama <oukhaiat@gmail.com>
2023-06-22 01:51:38 +0100

    fix typo

--- src/main/java/com/authentication/security/config/SecurityConfiguration.java
+++ src/main/java/com/authentication/security/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**", "/api/error")//White list authorized without token
+                .requestMatchers("/api/auth/**", "/api/error", "/api/users/**")//White list authorized without token
                 .permitAll()
                 .anyRequest()
                 .authenticated()//any other request should be authenticated
